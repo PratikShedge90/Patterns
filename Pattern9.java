1b33a57eba82606893734da819f6e5177b55d60d
Pratik <pratikshedge90@gmail.com>
2025-01-22 18:50:50 +0530

    Number Pattern Basic Codes

--- Pattern9.java
+++ Pattern9.java
@@ -3,14 +3,14 @@ public class Pattern9 {
         
         for(int i=1; i<=5; i++){
             for(int j=1; j<=i; j++){
-                System.out.print("_");
+                System.out.print(" ");
             }
             System.out.print("*");
             System.out.println();
         }
         for(int i=1; i<=4; i++){
             for(int j=4; j>=i; j--){
-                System.out.print("_");
+                System.out.print(" ");
             }
             System.out.print("*");
             System.out.println();
