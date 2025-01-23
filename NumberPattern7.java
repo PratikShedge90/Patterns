a9691773bf8e2cc459d460b93784cd93cf7f50db
Pratik <pratikshedge90@gmail.com>
2025-01-23 13:32:02 +0530

    New Added Number Patterns

---
+++ NumberPattern7.java
@@ -0,0 +1,18 @@
+public class NumberPattern7 {
+
+    public static void main(String[] args) {
+        
+        for(int i=1; i<=5; i++){
+            for(int j=i; j>=1; j--){
+                if(j%2==0){
+                    System.out.print("0 ");
+                }else{
+                    System.out.print("1 ");
+                }
+              //  System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    
+}
